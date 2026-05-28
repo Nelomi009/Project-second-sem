cc8aa807671426f54bfcd7e05a57f5a8bfab9119
neldori <nexusstail@gmail.com>
2026-05-28 16:31:47 +0700

    create CharacterMemento

--- RPG Character Editor/RPG Character Editor/CharacterMemento.cs
+++ RPG Character Editor/RPG Character Editor/CharacterMemento.cs
@@ -1,10 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace RpgCharacterEditor {
+  public class CharacterMemento {
+    public string Name { get; }
+    public string CharacterClass { get; }
+    public int Level { get; }
+    public int Health { get; }
+    public string Weapon { get; }
+    public string Armor { get; }
+    public int Gold { get; }
 
-namespace RPG_Character_Editor {
-  internal class CharacterMemento {
+    public CharacterMemento(
+        string name,
+        string characterClass,
+        int level,
+        int health,
+        string weapon,
+        string armor,
+        int gold) {
+      Name = name;
+      CharacterClass = characterClass;
+      Level = level;
+      Health = health;
+      Weapon = weapon;
+      Armor = armor;
+      Gold = gold;
+    }
   }
 }
